4e3b5f1f4d3def10a63a012c26747e17d4ce06f2
EricWangCN <wangzilong2016@outlook.com>
2022-05-26 20:09:19 +0800

    add logging

--- gpt2_tiny.sh
+++ gpt2_tiny.sh
@@ -1,5 +1,7 @@
 mkdir -p /home/v-zilong/fairseq_checkpoints
-fairseq-train /home/v-zilong/fairseq_data/wikipedia_en \
+mkdir -p /home/v-zilong/fairseq_logs
+
+python3 -m torch.distributed.launch --nproc_per_node=8 train.py /home/v-zilong/fairseq_data/wikipedia_en \
     --ddp-backend pytorch_ddp --fp16 --fp16-init-scale 4 \
     --checkpoint-activations \
     --task language_modeling --tokens-per-sample 256 --batch-size 8 \
@@ -7,4 +9,4 @@ fairseq-train /home/v-zilong/fairseq_data/wikipedia_en \
     --optimizer adam --adam-betas "(0.9,0.98)" \
     --lr 0.0001 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
     --max-update 50000 --log-format json --log-interval 100 \
-    --save-dir /home/v-zilong/fairseq_checkpoints
+    --save-dir /home/v-zilong/fairseq_checkpoints | tee -a /home/v-zilong/fairseq_checkpoints/gpt2_tiny.log
